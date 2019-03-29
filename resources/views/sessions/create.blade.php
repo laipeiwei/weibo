cfdf05388b4f4f6572ecff650377949af5be3efa
laipeiwei <laipeiweiCSDN@163.com>
2019-03-29 22:35:48 +0800

    modified:   app/Http/Controllers/SessionsController.php 	modified:   resources/views/sessions/create.blade.php

--- resources/views/sessions/create.blade.php
+++ resources/views/sessions/create.blade.php
@@ -18,7 +18,14 @@
                          <label for="password">密码：</label>
                          <input type="password" name="password" class="form-control" value="{{ old('password') }}">
                      </div>
-                     <button type="submit" class="btn btn-primary">登录</button>
+                    <div class="form-group">
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" name="remember" >
+                            <label class="form-check-label" for="exampleCheck1">记住我</label>
+                        </div>
+                    </div>
+
+                    <button type="submit" class="btn btn-primary">登录</button>
                 </form>
             <hr>
             <p>还没账号？<a href="{{ route('signup') }}">现在注册！</a></p>
