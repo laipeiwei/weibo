58d72c669e771fbe1d4bb97740312afc2977de6b
laipeiwei <laipeiweiCSDN@163.com>
2019-03-30 09:18:31 +0800

    提交个人中心编辑功能

--- resources/views/layouts/_header.blade.php
+++ resources/views/layouts/_header.blade.php
@@ -9,7 +9,7 @@
                         {{ Auth::user()->name }}</a>
                     <div class="dropdown-menu" aria-labelledby="navbarDropdown" style="float:right;">
                         <a class="dropdown-item" href="{{ route('users.show', Auth::user()) }}">个人中心</a>
-                            <a class="dropdown-item" href="#">编辑资料</a>
+                            <a class="dropdown-item" href="{{ route('users.edit' ,Auth::user())  }}">编辑资料</a>
                             <div class="dropdown-divider"></div>
                             <a class="dropdown-item" id="logout" href="#">
                                 <form action="{{ route('logout') }}" method="POST">
