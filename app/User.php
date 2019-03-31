8dab88a0160592e1d5083058d6f5c9ecb83291e3
laipeiwei <laipeiweiCSDN@163.com>
2019-03-31 19:53:42 +0800

    new file:   app/Http/Controllers/FollowersController.php 	modified:   app/Http/Controllers/UsersController.php 	modified:   app/Policies/UserPolicy.php 	modified:   app/User.php 	modified:   resources/views/shared/_stats.blade.php 	new file:   resources/views/users/_follow_form.blade.php 	modified:   resources/views/users/show.blade.php 	new file:   resources/views/users/show_follow.blade.php 	modified:   routes/web.php

--- app/User.php
+++ app/User.php
@@ -59,9 +59,14 @@ class User extends Authenticatable
     }
     public function feed()
     {
-        return  $this->statuses()
-            ->orderBy('created_at','desc');
+        $user_ids = $this->followings->pluck('id')->toArray();
+        var_dump($user_ids);exit();
+        array_push($user_ids, $this->id);
+        return Status::whereIn('user_id', $user_ids)
+            ->with('user')
+            ->orderBy('created_at', 'desc');
     }
+
 
     public function followers()
     {
